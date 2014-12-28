6d237218774e5712fa97d7edff8dd898e826c34c
CwnrRheswm <aramisgrey@gmail.com>
2014-12-28 12:30:14 -0800

    fix files and css

--- gruntfile.js
+++ gruntfile.js
@@ -1,15 +1,42 @@
 module.exports = function(grunt){
-	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+	//require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
+		locales: {
+			src: "dev/",
+			dest: "build/"
+		},
+		concat_css: {
 			options: {
 
 			},
-			build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+			all: {
+				src: ["dev/snowbird_main.css","dev/content/**/*.css","dev/components/**/*.css"],
+				dest: "dev/snowbird.css"
+			}
+		},
+		connect: {
+			server: {
+				options:{
+					base: "build/",
+					keepalive: false
+				}
+			},
+			polyDev: {
+				options: {
+					base: "dev/",
+					keepalive:true
+				}
+			}
+		},
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd: 'dev/',
+				src: 'snowbird.css',
+				dest: 'build/',
+				ext: '.min.css'
 			}
 		},
 		jade: {
@@ -17,42 +44,94 @@ module.exports = function(grunt){
 				options: {
 					pretty: true
 				},
-				files: [ {
+				files: [{
 					cwd: "dev",
-					src: "**/*.jade",
+					src: ["**/*.jade"],
 					dest: "build",
 					expand: true,
 					ext: ".html"
-				} ]
+				}]
 			}
 		},
 		jshint: {
 			options: {
 				force: true
 			},
-			files: ['gruntfile.js','sb.js']
+			files: ['gruntfile.js','dev/sb.js']
 		},
-		connect: {
-			server: {
-				options:{
-					port: 1337,
-				}
+		sass: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: "dev/",
+					src: ["**/*.scss"],
+					dest: "dev/SASSbuild/",
+					ext: ".css",
+					flatten: true
+				}]
+			}
+		},
+		uglify: {
+			build: {
+			options: {
+					beautify: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'dev/',
+					src: '**/*.js',
+					dest: 'build/',
+					ext: '.min.js'
+				}]
 			}
 		},
 		watch: {
-			jshint: {
-				files: ['<%= jshint.files %>'],
-				tasks: ['jshint']
+			livereload:{
+				options: {
+					livereload: true,
+				},
+				files: ['build/**/*']
+			},
+			concat: {
+				files: ["dev/**/*.css"],
+				tasks: ["concat_css:all"]
+			},
+			cssmin: {
+				files: ['dev/snowbird.css'],
+				tasks: ['cssmin:minify']
 			},
 			jade: {
-				files: "dev/**/*.jade",
-				tasks: "jade"
+				options: {
+					livereload: false,
+				},
+				files: ["dev/**/*.jade"],
+				tasks: ["jade:compile"],	
+			},
+			jshint:{
+				files: ['<%= jshint.files %>'],
+				tasks: ['jshint'],
+			},
+			uglify:{
+				files: ['dev/**/*.js'],
+				tasks: ['uglify:build']
 			}
 		}
 	});
 
-	grunt.registerTask('serve','connect:server');
-	grunt.registerTask('lint','jshint');
-	grunt.registerTask('compile','jade');
-	grunt.registerTask('default','watch');
+	
+	grunt.loadNpmTasks('grunt-concat-css');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('ugly','uglify:build');
+	grunt.registerTask('min',['cssmin:minify']);
+	grunt.registerTask('add',['concat_css']);
+	grunt.registerTask('start',['connect:server']);
+	grunt.registerTask('dev',['connect:server','watch']);
+	grunt.registerTask('polyDev',['connect:polyDev'])
 };
